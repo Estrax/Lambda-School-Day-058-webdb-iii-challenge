7334af81907be2d0684eb222d98b09d7852a8f33
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-27 16:13:28 -0400

    added basic routing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const app = require('express')();
 const initServerMiddleware = require('./src/serverMiddleware');
+const router = require('./src/routes');
 
 const port = process.env.PORT || 5000;
 const hostname = process.env.HOSTNAME || 'localhost';
 
 initServerMiddleware(app);
+app.use('/api', router);
 
 app.listen(port, (err) => {
     if(err) throw err;
